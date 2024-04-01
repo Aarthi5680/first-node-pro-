140e1d6b2d5c72fa8dda6588e9e3d7625bfc3e4e
Aarthi5680 <113370942+Aarthi5680@users.noreply.github.com>
2024-04-01 13:02:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
-//==//
+const fs = require('fs')
+fs.writeFileSync("arthi.txt","welcome arthi")
 
-let a= 20;
-if(a==20){
-    console.log(true)
-}else{
-    console.log(false)
-}
-
-//===//
-
-let b='35';
-
-if(b===35){
-    console.log(true)
-}else{
-    console.log(false)
-}
-
-//===//
-
-let c=35;
-
-if(c===35){
-    console.log(true)
-}else{
-    console.log(false)
-}
-
-
-//for looping statement//
-var x=20;
-
-for(i=0;i<20;i++){
-    console.log(i)
-
-}
-
-
+//creating file system using node.js
